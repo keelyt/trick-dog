8f13b0312df3dfad1d014bf1b02deb0278fe552f
Keely Timms <keely.a.timms@gmail.com>
2023-04-15 15:27:10 -0500

    Update ThemeContext to use local storage and OS preference

--- src/client/contexts/ThemeContext.tsx
+++ src/client/contexts/ThemeContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import type { ReactNode } from 'react';
 
 type ThemeMode = 'light' | 'dark';
@@ -8,16 +8,39 @@ interface ThemeContextType {
   toggleTheme: () => void;
 }
 
+/**
+ * Creates a new React context for the theme.
+ */
 export const ThemeContext = createContext<ThemeContextType>({
-  theme: 'light',
+  theme: 'dark',
   toggleTheme: (): void => undefined,
 });
 
+/**
+ * Provides the theme context to its child components.
+ * @param children The child components to be wrapped by the provider.
+ * @returns A React component that provides the theme context to its child components.
+ */
 export const ThemeProvider = ({ children }: { children: ReactNode }) => {
-  const [theme, setTheme] = useState<ThemeMode>('light');
+  // Use dark theme by default.
+  const [theme, setTheme] = useState<ThemeMode>('dark');
 
+  useEffect(() => {
+    // Get the theme from local storage.
+    const lsTheme = localStorage.getItem('ThemeContext:theme');
+    // Set the theme to the local storage theme if it is light/dark.
+    if (lsTheme === 'light' || lsTheme === 'dark') setTheme(lsTheme);
+    // Otherwise, use light theme if user's OS is in light mode.
+    else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches)
+      setTheme('light');
+  }, []);
+
+  /**
+   * Toggles the theme state and updates local storage.
+   */
   const toggleTheme = (): void => {
     setTheme(theme === 'light' ? 'dark' : 'light');
+    localStorage.setItem('ThemeContext:theme', theme);
   };
 
   return <ThemeContext.Provider value={{ theme, toggleTheme }}>{children}</ThemeContext.Provider>;
