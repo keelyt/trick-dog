1265cfeddb870b39aeab962037be05013bb02e89
Keely Timms <keely.a.timms@gmail.com>
2023-04-17 10:30:37 -0500

    Update import order eslint rule

--- src/client/contexts/ThemeContext.tsx
+++ src/client/contexts/ThemeContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, useState, useEffect } from 'react';
+
 import type { ReactNode } from 'react';
 
 type ThemeMode = 'light' | 'dark';
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
 /// <reference types="vite/client" />
 
 import { defineConfig } from 'vite';
+
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
