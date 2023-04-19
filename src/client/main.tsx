48b883bb35d4252525120a95091956776c93059c
Keely Timms <keely.a.timms@gmail.com>
2023-04-19 12:23:18 -0500

    Add react query

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import App from './App';
 import { ThemeProvider } from './contexts/ThemeContext';
 
 import './index.css';
+
+const queryClient: QueryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // Set default stale time to 5 minutes.
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
 
 const root: HTMLElement | null = document.getElementById('root');
 
@@ -12,7 +24,10 @@ if (root)
   createRoot(root).render(
     <StrictMode>
       <ThemeProvider>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+          <ReactQueryDevtools />
+        </QueryClientProvider>
       </ThemeProvider>
     </StrictMode>
   );
