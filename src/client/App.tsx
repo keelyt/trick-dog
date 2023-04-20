b77a44735cd1f9cccc8d7e8f882ca4633f9acf93
Keely Timms <keely.a.timms@gmail.com>
2023-04-19 20:14:17 -0500

    Add routes to App.tsx

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,17 +1,65 @@
-// import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+
+import PrivateRoute from './components/routing/PrivateRoute';
+import PublicRoute from './components/routing/PublicRoute';
+import Navbar from './components/ui/Navbar';
+import { useAuth } from './contexts/AuthContext';
 import { useTheme } from './contexts/ThemeContext';
+import Decks from './pages/Decks';
+import HomeLoggedIn from './pages/HomeLoggedIn';
+import HomeLoggedOut from './pages/HomeLoggedOut';
+import Login from './pages/Login';
+import PageNotFound from './pages/PageNotFound';
+import Profile from './pages/Profile';
+import Study from './pages/Study';
 
 import styles from './App.module.scss';
 
 export default function App() {
-  const { theme, toggleTheme } = useTheme();
+  const { theme } = useTheme();
+  // TODO: authed set to true for development--change to useAuth after implementing auth
+  // const { authed } = useAuth();
+  const authed = true;
 
   return (
     <div className={`${styles.container} ${styles[`theme--${theme}`]}`}>
-      <button className={styles.btn} type='button' onClick={toggleTheme}>
-        Toggle
-      </button>
-      <div>hi</div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={authed ? <HomeLoggedIn /> : <HomeLoggedOut />} />
+        <Route
+          path='/decks'
+          element={
+            <PrivateRoute>
+              <Decks />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path='/study'
+          element={
+            <PrivateRoute>
+              <Study />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path='/profile'
+          element={
+            <PrivateRoute>
+              <Profile />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path='/login'
+          element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>
+          }
+        />
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
     </div>
   );
 }
