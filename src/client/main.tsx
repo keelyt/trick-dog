f20daa81c7bc42337931ed21e8a9cba02e16eeb3
Keely Timms <keely.a.timms@gmail.com>
2023-04-22 15:02:36 -0500

    Add missing AuthProvider to main

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import App from './App';
+import { AuthProvider } from './contexts/AuthContext';
 import { ThemeProvider } from './contexts/ThemeContext';
 
 import './styles/GlobalStyles.scss';
@@ -24,13 +25,15 @@ const root: HTMLElement | null = document.getElementById('root');
 if (root)
   createRoot(root).render(
     <StrictMode>
-      <ThemeProvider>
-        <QueryClientProvider client={queryClient}>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-          <ReactQueryDevtools />
-        </QueryClientProvider>
-      </ThemeProvider>
+      <AuthProvider>
+        <ThemeProvider>
+          <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+              <App />
+            </BrowserRouter>
+            <ReactQueryDevtools />
+          </QueryClientProvider>
+        </ThemeProvider>
+      </AuthProvider>
     </StrictMode>
   );
