026877add23d3f610802dc40d78f80c583c91a2e
Keely Timms <keely.a.timms@gmail.com>
2023-04-25 15:51:21 -0500

    Update Modal styles

--- src/client/components/ui/Modal.tsx
+++ src/client/components/ui/Modal.tsx
@@ -2,6 +2,7 @@ import { useRef } from 'react';
 
 import useEscapeKey from '../../helpers/useEscapeKey';
 import useOutsideClick from '../../helpers/useOutsideClick';
+import useTabFocus from '../../helpers/useTabFocus';
 
 import styles from './Modal.module.scss';
 
@@ -14,12 +15,13 @@ export default function Modal({
   onClose: () => void;
   children: ReactNode;
 }): JSX.Element {
-  const innerModalRef = useRef(null);
+  const innerModalRef = useRef<HTMLDivElement>(null);
   useOutsideClick(onClose, innerModalRef);
   useEscapeKey(onClose);
+  useTabFocus(innerModalRef);
 
   return (
-    <div className={styles.modal}>
+    <div className={styles.modal} tabIndex={-1} aria-modal='true' role='dialog'>
       <div ref={innerModalRef} className={styles.modal__inner}>
         {children}
       </div>
