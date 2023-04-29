78504f054ae0a6c2f2f4356f6b0cacca34110c34
Keely Timms <keely.a.timms@gmail.com>
2023-04-28 20:06:18 -0500

    Remove non-li elements from EditDecks ul and add back button

--- src/client/pages/EditDeck.tsx
+++ src/client/pages/EditDeck.tsx
@@ -5,9 +5,12 @@ import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 
 import CardView from '../components/card/CardView';
+import BackButton from '../components/ui/BackButton';
 import LoadingSpinner from '../components/ui/LoadingSpinner';
 import fetchWithError from '../helpers/fetchWithError';
 import useCardsInfiniteQuery from '../helpers/useCardsInfiniteQuery';
+
+import styles from './EditDeck.module.scss';
 
 import type { CardData, DeckData } from '../types';
 
@@ -17,6 +20,8 @@ import type { CardData, DeckData } from '../types';
 
 // TODO: For now, will only give ability to sort newest to oldest.
 // If want to add ability to sort oldest to newest, will need to modify query.
+
+// TODO: Adjust styling to use partially persistent nav instead of fixed-size content window.
 
 export default function EditDeck(): JSX.Element {
   // Get the deckId from the URL.
@@ -43,24 +48,38 @@ export default function EditDeck(): JSX.Element {
   }, [inView, cardsQuery.fetchNextPage, cardsQuery.hasNextPage]);
 
   return (
-    <main role='main'>
-      EditDeck {deckId}
-      <ul>
-        {cardsQuery.isLoading ? (
-          <p>Loading...</p>
-        ) : (
-          cardsQuery.data?.pages.map((page) =>
-            page.map((card, i) => (
-              <CardView
-                key={card.id}
-                ref={i === page.length - 1 ? ref : undefined}
-                question={card.question}
-              />
-            ))
-          )
-        )}
-        {cardsQuery.isFetchingNextPage ? <LoadingSpinner /> : null}
-      </ul>
-    </main>
+    <div className={styles.container}>
+      <main role='main' className={styles.content}>
+        <div className={styles.top}>
+          <div>
+            <BackButton href='/decks' label='Back to all decks' />
+          </div>
+          <div>
+            <h1>{deckQuery.isSuccess ? `${deckQuery.data.deck_name}` : 'Deck'}</h1>
+          </div>
+        </div>
+        <div className={styles['list-container']}>
+          {cardsQuery.isError && cardsQuery.error instanceof Error && (
+            <span>{cardsQuery.error.message}</span>
+          )}
+          {cardsQuery.isLoading ? (
+            <p>Loading...</p>
+          ) : (
+            <ul className={styles.list}>
+              {cardsQuery.data?.pages.map((page) =>
+                page.map((card, i) => (
+                  <CardView
+                    key={card.id}
+                    ref={i === page.length - 1 ? ref : undefined}
+                    question={card.question}
+                  />
+                ))
+              )}
+            </ul>
+          )}
+          {cardsQuery.isFetchingNextPage ? <LoadingSpinner /> : null}
+        </div>
+      </main>
+    </div>
   );
 }
