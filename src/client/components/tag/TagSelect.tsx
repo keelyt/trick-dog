268c4de69036f4fe328f5b43bc0c413d49e3a8d9
Keely Timms <keely.a.timms@gmail.com>
2023-05-01 14:10:36 -0500

    Add TagSelect component to filter EditDeck cards by tag

---
+++ src/client/components/tag/TagSelect.tsx
@@ -0,0 +1,28 @@
+import styles from './TagSelect.module.scss';
+
+import type { TagData } from '../../../types';
+import type { ChangeEvent } from 'react';
+
+interface TagSelectProps {
+  tags: TagData[];
+  onChange: (event: ChangeEvent<HTMLSelectElement>) => void;
+}
+
+export default function TagSelect({ tags = [], onChange }: TagSelectProps): JSX.Element | null {
+  if (tags.length === 0) return null;
+  return (
+    <>
+      <label htmlFor='tag' className={styles['visually-hidden']}>
+        Filter by Tag
+      </label>
+      <select id='tag' name='tag' onChange={onChange}>
+        <option value=''>Select a tag to filter</option>
+        {tags.map((tag) => (
+          <option key={tag.id} value={tag.id}>
+            {tag.tag_name}
+          </option>
+        ))}
+      </select>
+    </>
+  );
+}
