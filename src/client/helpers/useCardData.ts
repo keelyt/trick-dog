d8bf10830e25ee71973f79066873f949d5396f16
Keely Timms <keely.a.timms@gmail.com>
2023-05-11 16:03:47 -0500

    Add useCardData for fetching card data with React Query

---
+++ src/client/helpers/useCardData.ts
@@ -0,0 +1,23 @@
+import { useQuery } from '@tanstack/react-query';
+
+import fetchWithError from './fetchWithError';
+
+import type { CardData, CardResponse } from '../../types';
+
+/**
+ * A React hook that uses React Query to fetch data for a specific card.
+ * @param deckId The ID of the deck to fetch data for.
+ * @param cardId The ID of the card to fetch data for.
+ * @returns The result of the query.
+ */
+export default function useCardData(deckId: number, cardId: number) {
+  return useQuery({
+    queryKey: ['decks', deckId, 'cards', cardId],
+    queryFn: async ({ signal }): Promise<CardData> => {
+      const result = await fetchWithError<CardResponse>(`/api/decks/${deckId}/cards/${cardId}`, {
+        signal,
+      });
+      return result.card;
+    },
+  });
+}
