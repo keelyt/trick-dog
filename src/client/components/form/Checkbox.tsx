d8f91d8c89638fcdafc801a8b6efc67157ad022d
Keely Timms <keely.a.timms@gmail.com>
2023-05-17 20:57:15 -0500

    Style checked checkboxes with CSS instead of using state for dynamic class name

--- src/client/components/form/Checkbox.tsx
+++ src/client/components/form/Checkbox.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-
 import styles from './Checkbox.module.scss';
 
 import type { UseFormRegister, FieldValues, RegisterOptions, Path } from 'react-hook-form';
@@ -21,16 +19,13 @@ export default function Checkbox<TFormValues extends FieldValues>({
   id,
   validation = {},
 }: FormInputProps<TFormValues>): JSX.Element {
-  const [checked, setChecked] = useState<boolean>(false);
-
   return (
-    <div className={`${styles.checkbox} ${checked ? styles['checkbox--checked'] : ''}`}>
+    <div className={`${styles.checkbox}`}>
       <input
         type='checkbox'
         id={id}
         value={value}
         {...register(name, validation)}
-        onChange={(event) => setChecked(event.target.checked)}
         className={styles.checkbox__input}
       />
       <label htmlFor={id} className={styles.checkbox__label}>
