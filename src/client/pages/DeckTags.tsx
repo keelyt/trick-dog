450d8e6531a5b3e17c86ca0026e16e78f7bbccf3
Keely Timms <keely.a.timms@gmail.com>
2023-05-24 19:52:14 -0500

    Show mutation errors when adding tags and hide errors when input is cleared

--- src/client/pages/DeckTags.tsx
+++ src/client/pages/DeckTags.tsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 
+import FormError from '../components/form/FormError';
 import TextInput from '../components/form/TextInput';
 import DeckTagItem from '../components/tag/DeckTagItem';
 import RenameTagForm from '../components/tag/RenameTagForm';
@@ -28,6 +29,7 @@ export default function DeckTags(): JSX.Element {
   const [modalSourceRef, setModalSourceRef] =
     useState<MutableRefObject<HTMLButtonElement | null> | null>(null);
   const [openModal, setOpenModal] = useState<null | 'delete' | 'rename'>(null);
+  const [showErrors, setShowErrors] = useState<boolean>(true);
 
   const deleteTag = useDeleteTag();
   const addTag = useAddTag();
@@ -37,11 +39,18 @@ export default function DeckTags(): JSX.Element {
     handleSubmit,
     reset,
     formState: { errors, isValid },
-  } = useForm<FormValues>();
+  } = useForm<FormValues>({ mode: 'onChange' });
 
   // Handler function for form submission
   const onSubmit: SubmitHandler<FormValues> = (data: FormValues) =>
     addTag.mutate({ deckId, tagName: data.tagName }, { onSuccess: () => reset() });
+
+  // Hide errors when input is cleared.
+  useEffect(() => {
+    addTag.reset();
+    if (errors.tagName?.type === 'required') setShowErrors(false);
+    else setShowErrors(true);
+  }, [errors.tagName]);
 
   const resetState = () => {
     modalSourceRef?.current?.focus();
@@ -59,7 +68,7 @@ export default function DeckTags(): JSX.Element {
 
   return (
     <div className={styles['tags-container']}>
-      <h2 className={styles.heading}>Manage Deck Tags</h2>
+      {/* <h2 className={styles.heading}>Manage Deck Tags</h2> */}
       {openModal && targetTag && (
         <Modal onClose={resetState}>
           {openModal === 'delete' && (
@@ -82,25 +91,29 @@ export default function DeckTags(): JSX.Element {
           )}
         </Modal>
       )}
-      <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
-        <TextInput
-          register={register}
-          name='tagName'
-          label='New Tag'
-          errors={errors}
-          validation={{ required: true, maxLength: 50 }}
-        />
-        <Button
-          as='button'
-          type='submit'
-          size='lg'
-          rounded={false}
-          disabled={addTag.isLoading || !isValid}
-        >
-          {addTag.isLoading ? 'Adding...' : 'Add Tag'}
-        </Button>
-      </form>
-      <ul>
+      <div className={styles.form__container}>
+        <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+          <TextInput
+            register={register}
+            name='tagName'
+            label='New Tag'
+            errors={errors}
+            validation={{ required: true, maxLength: 50 }}
+            showErrors={showErrors}
+          />
+          <Button
+            as='button'
+            type='submit'
+            size='lg'
+            rounded={false}
+            disabled={addTag.isLoading || !isValid}
+          >
+            {addTag.isLoading ? 'Adding...' : 'Add Tag'}
+          </Button>
+        </form>
+        {addTag.isError && <FormError errorMessage={addTag.error.message} />}
+      </div>
+      <ul className={styles.list}>
         {deckTags.map((tag) => (
           <DeckTagItem
             key={tag.id}
