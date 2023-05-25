207f60d8a812fdf8fc32150190ca481166ba1df4
Keely Timms <keely.a.timms@gmail.com>
2023-05-24 20:13:46 -0500

    Add throttle to useScrollListener hook to reduce rerenders of navbar

--- src/client/helpers/useScrollListener.ts
+++ src/client/helpers/useScrollListener.ts
@@ -1,5 +1,13 @@
 import { useEffect, useState } from 'react';
 
+/**
+ * A React hook that listens to scroll events and returns scroll data.
+ * @returns scrollData Object containing scroll position data.
+ * @returns scrollData.x Current horizontal scroll position.
+ * @returns scrollData.y Current vertical scroll position.
+ * @returns scrollData.prevX Previous horizontal scroll position.
+ * @returns scrollData.prevY Previous vertical scroll position.
+ */
 export default function useScrollListener() {
   const [scrollData, setScrollData] = useState({ x: 0, y: 0, prevX: 0, prevY: 0 });
 
@@ -13,10 +21,24 @@ export default function useScrollListener() {
       }));
     // Call the handler when the component mounts to set the state.
     handleScroll();
+    // Throttle the scroll handler with a wait of 100ms.
+    const throttledHandleScroll = (() => {
+      let wait = false;
+
+      return () => {
+        if (wait) return;
+
+        handleScroll();
+        wait = true;
+        setTimeout(() => {
+          wait = false;
+        }, 100);
+      };
+    })();
     // Add scroll event listener.
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('scroll', throttledHandleScroll);
     // Cleanup function to remove the event listener when the component unmounts.
-    return () => window.removeEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', throttledHandleScroll);
   }, []);
 
   return scrollData;
