9a75ad9f5340bef12ffdfb29e95b48ebe0afeadb
Keely Timms <keely.a.timms@gmail.com>
2023-06-04 15:45:57 -0500

    Finish StudyReviewer functionality except keyboard shortcuts, no styling

--- src/client/pages/StudyReviewer.tsx
+++ src/client/pages/StudyReviewer.tsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 
 import BackButton from '../components/ui/BackButton';
+import Button from '../components/ui/Button';
+import LoadingIndicator from '../components/ui/LoadingIndicator';
+import LoadingSpinner from '../components/ui/LoadingSpinner';
 import QueryError from '../components/ui/QueryError';
 import useGetStudyCards from '../helpers/useGetStudyCards';
 import useUpdateCardDifficulty from '../helpers/useUpdateCardDifficulty';
@@ -9,30 +12,46 @@ import styles from './StudyReviewer.module.scss';
 
 export default function StudyReviewer({ selection }: { selection: string }) {
   const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const [answerRevealed, setAnswerRevealed] = useState<boolean>(false);
+  const [mutatingLast, setMutatingLast] = useState<boolean>(false);
 
   const cardsQuery = useGetStudyCards(selection);
   const updateDifficulty = useUpdateCardDifficulty();
 
-  const handleSubmit = (deckId: number, cardId: number, difficulty: string) => {
-    const shouldRefetch = !cardsQuery.data || currentIndex >= cardsQuery.data.length - 1;
-    if (!shouldRefetch) setCurrentIndex((prevIndex) => prevIndex + 1);
+  const handleSubmit = (difficulty: string) => {
+    if (!answerRevealed || !cardsQuery.data || !cardsQuery.data[currentIndex] || mutatingLast)
+      return;
+    const shouldRefetch = currentIndex >= cardsQuery.data.length - 1;
+    // Keep track of status of mutation of last index (to prevent duplicate requests when on last index).
+    if (shouldRefetch) setMutatingLast(true);
 
     updateDifficulty.mutate(
-      { deckId, cardId, difficulty },
+      {
+        deckId: cardsQuery.data[currentIndex].deckId,
+        cardId: cardsQuery.data[currentIndex].id,
+        difficulty,
+      },
       {
         onSettled: () => {
           if (shouldRefetch) {
             void cardsQuery.refetch();
             setCurrentIndex(0);
+            setMutatingLast(false);
+            setAnswerRevealed(false);
           }
         },
       }
     );
+
+    if (!shouldRefetch) {
+      setCurrentIndex((prevIndex) => prevIndex + 1);
+      setAnswerRevealed(false);
+    }
   };
 
   return (
     <main className={styles.content}>
-      <BackButton href='/study' label='ModifySelection' state={{ selection }} />
+      <BackButton href='/study' label='Modify Selection' state={{ selection }} />
       {cardsQuery.isError && (
         <QueryError
           label='Error retrieving information from server.'
@@ -42,28 +61,36 @@ export default function StudyReviewer({ selection }: { selection: string }) {
       {updateDifficulty.isError && (
         <div>There was an error updating the previous card&apos;s difficulty on the server.</div>
       )}
-      {cardsQuery.isLoading && <div>Loading...</div>}
-      {cardsQuery.isRefetching && <div>Fetching more cards...</div>}
-      {cardsQuery.isSuccess && !cardsQuery.isRefetching && (
+      {(cardsQuery.isLoading || cardsQuery.isRefetching || mutatingLast) &&
+        (cardsQuery.isLoading ? <LoadingIndicator /> : <LoadingSpinner />)}
+      {cardsQuery.isSuccess && !cardsQuery.isRefetching && !mutatingLast && (
         <>
           <div>{cardsQuery.data[currentIndex].question}</div>
-          <div>{cardsQuery.data[currentIndex].answer}</div>
-          {['Easy', 'Medium', 'Hard'].map((difficulty) => (
-            <button
-              key={difficulty}
-              onClick={() =>
-                handleSubmit(
-                  cardsQuery.data[currentIndex].deckId,
-                  cardsQuery.data[currentIndex].id,
-                  difficulty
-                )
-              }
-            >
-              {difficulty}
-            </button>
-          ))}
+          {answerRevealed && <div>{cardsQuery.data[currentIndex].answer}</div>}
         </>
       )}
+      {!answerRevealed && (
+        <Button
+          as='button'
+          type='button'
+          colorScheme='secondary'
+          onClick={() => setAnswerRevealed(true)}
+        >
+          Show Answer
+        </Button>
+      )}
+      {answerRevealed &&
+        ['Easy', 'Medium', 'Hard'].map((difficulty) => (
+          <Button
+            key={difficulty}
+            as='button'
+            type='button'
+            colorScheme='secondary'
+            onClick={() => handleSubmit(difficulty)}
+          >
+            {difficulty}
+          </Button>
+        ))}
     </main>
   );
 }
