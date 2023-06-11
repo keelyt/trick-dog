6e2b6975f19617f061416325aab3c4f8dfb66498
Keely Timms <keely.a.timms@gmail.com>
2023-06-11 12:09:59 -0500

    Finish /login and /status in authRoutes

--- src/server/routes/authRoutes.ts
+++ src/server/routes/authRoutes.ts
@@ -1,12 +1,26 @@
 import { Router } from 'express';
 
+import { sessionController } from '../controllers/sessionController';
 import { userController } from '../controllers/userController';
 import verifyGoogleToken from '../middleware/verifyGoogleToken';
 
+import type { ReqBodyLogin, ResLocals, ResLocalsLogin } from '../types';
+
 const authRouter = Router();
 
-authRouter.post('/login', verifyGoogleToken, userController.verifyOrAddUser, (req, res) => {
-  return res.status(200).json({ userInfo: res.locals.userInfo });
+authRouter.get<unknown, unknown, unknown, unknown, ResLocals>('/status', (req, res) => {
+  const authed = !!res.locals.userId;
+  return res.status(200).json({ authed });
 });
+
+authRouter.post<unknown, unknown, ReqBodyLogin, unknown, ResLocalsLogin>(
+  '/login',
+  verifyGoogleToken,
+  userController.verifyOrAddUser,
+  sessionController.createSession,
+  (req, res) => {
+    return res.status(200).json({ userInfo: res.locals.userInfo });
+  }
+);
 
 export default authRouter;
