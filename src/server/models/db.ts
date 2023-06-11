bb8bd307996168c3668b41a056e69c21cd73fad8
Keely Timms <keely.a.timms@gmail.com>
2023-06-11 10:47:43 -0500

    Add getSessionStore to pg database set up for use with express-session

--- src/server/models/db.ts
+++ src/server/models/db.ts
@@ -1,9 +1,12 @@
 import { config } from 'dotenv';
 config();
 
+import connectPgSimple from 'connect-pg-simple';
 import pkg from 'pg';
 const { Pool } = pkg;
 
+import type { RequestHandler } from 'express';
+import type { SessionOptions } from 'express-session';
 import type { QueryResult, QueryResultRow } from 'pg';
 
 const pool = new Pool({
@@ -15,4 +18,9 @@ export function query<R extends QueryResultRow>(
   params: unknown[] = []
 ): Promise<QueryResult<R>> {
   return pool.query(queryText, params);
+}
+
+export function getSessionStore(session: (options?: SessionOptions) => RequestHandler) {
+  const pgSession = connectPgSimple(session);
+  return new pgSession({ pool, tableName: 'sessions', createTableIfMissing: true });
 }
