a57dc2a4cc716a290b348a9a8e3d2e8ad64070c0
Keely Timms <keely.a.timms@gmail.com>
2023-06-15 00:10:02 -0500

    Update useDeleteDeck and useRenameDeck to match api responses

--- src/client/helpers/useRenameDeck.ts
+++ src/client/helpers/useRenameDeck.ts
@@ -2,7 +2,7 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import fetchWithError from './fetchWithError';
 
-import type { DeckData, DeckResponse } from '../../types';
+import type { DeckData, DeckPatchResponse } from '../../types';
 
 interface RenameDeckParams {
   deckId: number;
@@ -14,20 +14,24 @@ export default function useRenameDeck() {
 
   return useMutation({
     mutationFn: async ({ deckId, deckName }: RenameDeckParams) =>
-      fetchWithError<DeckResponse>(`/api/decks/${deckId}`, {
+      fetchWithError<DeckPatchResponse>(`/api/decks/${deckId}`, {
         method: 'PATCH',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({ deckName }),
       }),
-    onSuccess: (data: DeckResponse) => {
+    onSuccess: (data: DeckPatchResponse) => {
       // Update the decks list with the updated deck.
       queryClient.setQueryData(['decks'], (old: DeckData[] | undefined) =>
-        old?.map((deck) => (deck.id === data.deck.id ? data.deck : deck))
+        old?.map((deck) => (deck.id === data.deck.id ? Object.assign(deck, data.deck) : deck))
       );
       // Update the deck in the cache.
-      queryClient.setQueryData(['decks', data.deck.id], data.deck);
+      queryClient.setQueryData(['decks', data.deck.id], (old: DeckData | undefined) => ({
+        ...data.deck,
+        cardCount: old?.cardCount ?? 0,
+        tags: old?.tags ?? [],
+      }));
     },
   });
 }
