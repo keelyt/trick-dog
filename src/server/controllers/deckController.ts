60e299f60a1da893b35cff2113fba5c817de3271
Keely Timms <keely.a.timms@gmail.com>
2023-06-19 15:19:03 -0500

    Modularize deck queries

--- src/server/controllers/deckController.ts
+++ src/server/controllers/deckController.ts
@@ -1,10 +1,15 @@
 import createError from 'http-errors';
 
-import { query } from '../database/db';
+import {
+  deleteDeckQuery,
+  insertDeckQuery,
+  selectDeckQuery,
+  selectDecksQuery,
+  updateDeckQuery,
+} from '../database/deckQueries';
 import asyncMiddleware from '../utils/asyncMiddleware';
 import createErrorLog from '../utils/createErrorLog';
 
-import type { DeckData } from '../../types';
 import type {
   ReqBodyDeck,
   ReqParamsDeck,
@@ -35,15 +40,8 @@ const addDeck = asyncMiddleware<unknown, unknown, ReqBodyDeck, unknown, ResLocal
         })
       );
 
-    const queryString = `
-    INSERT INTO decks (deck_name, user_id)
-    VALUES ($1, $2)
-    RETURNING id, deck_name AS "deckName";
-    `;
-    const queryParams = [deckName, userId];
-
     try {
-      const deck = await query<Pick<DeckData, 'id' | 'deckName'>>(queryString, queryParams);
+      const deck = await insertDeckQuery({ deckName, userId });
       res.locals.deck = { ...deck.rows[0], cardCount: 0, tags: [] };
       return next();
     } catch (error) {
@@ -74,62 +72,12 @@ const deleteDeck = asyncMiddleware<ReqParamsDeck, unknown, unknown, unknown, Res
         })
       );
 
-    // The database was configured with cascading deletes, so only need to delete from decks table.
-    const queryString = `
-    DELETE from decks
-    WHERE user_id = $1 AND id = $2;
-    `;
-    const queryParams = [userId, deckId];
-
     try {
-      const deck = await query(queryString, queryParams);
+      const deck = await deleteDeckQuery({ userId, deckId });
       if (!deck.rowCount)
         return next(
           createError(404, 'Deck not found.', { log: createErrorLog(method, 'Deck not found.') })
         );
-      return next();
-    } catch (error) {
-      return next(
-        createError(500, errMessage, {
-          log: createErrorLog(
-            method,
-            error instanceof Error ? error.message : 'Unknown database error.'
-          ),
-        })
-      );
-    }
-  }
-);
-
-const getDecks = asyncMiddleware<unknown, unknown, unknown, unknown, ResLocalsDecks>(
-  async (req, res, next) => {
-    const method = 'deckController.getDecks';
-    const errMessage = 'Error retrieving decks from server.';
-
-    const { userId } = res.locals;
-
-    const queryString = `
-    SELECT dc.*, ct.tags
-    FROM (
-      SELECT d.id, d.deck_name AS "deckName", COUNT(c.id) AS "cardCount"
-      FROM decks d
-      LEFT OUTER JOIN cards c
-      ON c.deck_id = d.id
-      WHERE d.user_id = $1
-      GROUP BY d.id
-    ) dc
-    LEFT OUTER JOIN (
-      SELECT deck_id, json_agg(json_build_object('id', id, 'tagName', tag_name, 'deckId', deck_id)) AS tags
-      FROM tags
-      GROUP BY deck_id
-    ) ct
-    ON ct.deck_id = dc.id;
-    `;
-    const queryParams = [userId];
-
-    try {
-      const decks = await query<DeckData>(queryString, queryParams);
-      res.locals.decks = decks.rows;
       return next();
     } catch (error) {
       return next(
@@ -159,33 +107,37 @@ const getDeck = asyncMiddleware<ReqParamsDeck, unknown, unknown, unknown, ResLoc
         })
       );
 
-    const queryString = `
-    SELECT dc.*, ct.tags
-    FROM (
-      SELECT d.id, d.deck_name AS "deckName", COUNT(c.id) AS "cardCount"
-      FROM decks d
-      LEFT OUTER JOIN cards c
-      ON c.deck_id = d.id
-      WHERE d.user_id = $1 AND d.id = $2
-      GROUP BY d.id
-    ) dc
-    LEFT OUTER JOIN (
-      SELECT deck_id, json_agg(json_build_object('id', id, 'tagName', tag_name, 'deckId', deck_id)) AS tags
-      FROM tags
-      WHERE deck_id = $2
-      GROUP BY deck_id
-    ) ct
-    ON ct.deck_id = dc.id;
-    `;
-    const queryParams = [userId, deckId];
-
     try {
-      const deck = await query<DeckData>(queryString, queryParams);
+      const deck = await selectDeckQuery({ userId, deckId });
       if (!deck.rows.length)
         return next(
           createError(404, 'Deck not found.', { log: createErrorLog(method, 'Deck not found.') })
         );
       res.locals.deck = deck.rows[0];
+      return next();
+    } catch (error) {
+      return next(
+        createError(500, errMessage, {
+          log: createErrorLog(
+            method,
+            error instanceof Error ? error.message : 'Unknown database error.'
+          ),
+        })
+      );
+    }
+  }
+);
+
+const getDecks = asyncMiddleware<unknown, unknown, unknown, unknown, ResLocalsDecks>(
+  async (req, res, next) => {
+    const method = 'deckController.getDecks';
+    const errMessage = 'Error retrieving decks from server.';
+
+    const { userId } = res.locals;
+
+    try {
+      const decks = await selectDecksQuery(userId);
+      res.locals.decks = decks.rows;
       return next();
     } catch (error) {
       return next(
@@ -235,16 +187,8 @@ const updateDeck = asyncMiddleware<
       })
     );
 
-  const queryString = `
-    UPDATE decks
-    SET deck_name = $1
-    WHERE user_id = $2 AND id = $3
-    RETURNING id, deck_name AS "deckName";
-    `;
-  const queryParams = [deckName, userId, deckId];
-
   try {
-    const deck = await query<Pick<DeckData, 'id' | 'deckName'>>(queryString, queryParams);
+    const deck = await updateDeckQuery({ deckName, userId, deckId });
     if (!deck.rows.length)
       return next(
         createError(404, 'Deck not found.', { log: createErrorLog(method, 'Deck not found.') })
