887f90eefa52ca813a946fa1e2ae03609179d9bf
Keely Timms <keely.a.timms@gmail.com>
2023-07-13 15:31:18 -0400

    Add home page for unauthenticated users

--- src/client/pages/HomeLoggedOut.tsx
+++ src/client/pages/HomeLoggedOut.tsx
@@ -1,10 +1,29 @@
+import { ReactComponent as Logo } from '../assets/logo-noBg.svg';
 import GoogleLoginButton from '../components/authentication/GoogleLoginButton';
+
+import styles from './HomeLoggedOut.module.scss';
 
 export default function HomeLoggedOut() {
   return (
-    <div>
-      Welcome, non-user
-      <GoogleLoginButton />
-    </div>
+    <main className={styles.content}>
+      <div className={styles.text}>
+        <h1 className={styles.heading}>Trick Dog</h1>
+        <h2 className={styles.subheading}>Learn Fast, Learn Well</h2>
+        <p className={styles.details}>
+          Trick Dog&apos;s online flashcards help you master any subject with its spaced repetition
+          algorithm and efficient, intuitive interface.
+        </p>
+        <GoogleLoginButton />
+      </div>
+      <Logo className={styles.logo} />
+      <a
+        className={styles.github}
+        href='https://github.com/keelyt/trick-dog'
+        target='_blank'
+        rel='noreferrer noopener'
+      >
+        View on GitHub
+      </a>
+    </main>
   );
 }
