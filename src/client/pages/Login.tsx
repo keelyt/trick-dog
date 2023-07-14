ee8bcc6a9826303a8b75ee598b1626523208c565
Keely Timms <keely.a.timms@gmail.com>
2023-07-14 15:55:32 -0400

    Complete styled login page

--- src/client/pages/Login.tsx
+++ src/client/pages/Login.tsx
@@ -1,10 +1,17 @@
+import { ReactComponent as Logo } from '../assets/logo-noBg.svg';
 import GoogleLoginButton from '../components/authentication/GoogleLoginButton';
+
+import styles from './Login.module.scss';
 
 export default function Login(): JSX.Element {
   return (
-    <div>
-      <h1>Login</h1>
+    <main className={styles.content}>
+      <Logo className={styles.logo} />
+      <span className={styles.heading}>Trick Dog</span>
+      <span className={styles.subheading}>Learn Fast, Learn Well</span>
+      <hr className={styles.divider} />
+      <span className={styles.text}>Sign in to continue</span>
       <GoogleLoginButton />
-    </div>
+    </main>
   );
 }
